a876b01def4d0d7ab9ed4ecaa3e3f4cb4881d92e
Francisco Gonzalez <franciscogonzalez@Franciscos-MacBook-Pro.local>
2019-10-29 09:03:13 -0400

    Updates

---
+++ precache-manifest.583a16bd8216db87c01cfc2a3b7a9301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49cc2d49805005aed3003ea1769f6f6",
+    "url": "/memorygamereact/index.html"
+  },
+  {
+    "revision": "860310f9f22f1a4c172d",
+    "url": "/memorygamereact/static/css/main.c79c4e66.chunk.css"
+  },
+  {
+    "revision": "e8c3860e98ffb698a0c9",
+    "url": "/memorygamereact/static/js/2.ae0e2c1f.chunk.js"
+  },
+  {
+    "revision": "860310f9f22f1a4c172d",
+    "url": "/memorygamereact/static/js/main.45880ec5.chunk.js"
+  },
+  {
+    "revision": "10cf4a71e0401c8bf209",
+    "url": "/memorygamereact/static/js/runtime-main.6c803b95.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/memorygamereact/static/media/logo.25bf045c.svg"
+  }
+]);
